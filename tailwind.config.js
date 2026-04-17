05ec77c97041fb69695bf2fabf1c9e938e8d2334
Youraj Singh <ys493587@gmail.com>
2026-04-17 11:16:49 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6D00',
+        'primary-dark': '#E65100',
+      },
+    },
+  },
+  plugins: [],
+}
