4e928a01eef3db3eb9910c54b66b9371b409ec37
yuvraj67-ys <ys493587@gmail.com>
2026-06-29 13:11:16 +0530

    fix: clone route import path (5 levels up)

--- app/api/vehicles/[id]/clone/route.js
+++ app/api/vehicles/[id]/clone/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
-import { addVehicle } from '../../../../lib/db';
-import admin from '../../../../lib/firebase-admin';
+import { addVehicle } from '../../../../../lib/db';
+import admin from '../../../../../lib/firebase-admin';
 
 export async function POST(request, { params }) {
   try {
